131f195b978e5049b3bc336bee6e9d01cfb8fa93
thomhie0416 <48708193+thomhie0416@users.noreply.github.com>
2020-01-15 19:37:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 	<title>Uploads FILES</title>
 </head>
 <body>
+<center>
 	<a href="uploadtoserver.php">
-		<button>Click ME!</button>
-	</a>
+		<button style="border:1;width: 500px;height: 200px; font-size:300%">Click ME!</button>
+	</a></center>
 </body>
 </html>
